5b1a76ed2bd586eaff470f328f1bb0e8788f291a
huangyangfeng <huangyangfeng@thunisoft.com>
2017-02-07 15:12:58 +0800

    EGit!

--- GitStudy/src/study/Git/HelloGit.java
+++ GitStudy/src/study/Git/HelloGit.java
@@ -3,5 +3,7 @@ package study.Git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Say Hello, Git!");
+		System.out.println("Say Hello, Git!");
+		System.out.println("Say Hello, Git!");
 	}
 }
---
+++ GitStudy/src/study/Git/TestEgit.java
@@ -0,0 +1,5 @@
+package study.Git;
+
+public class TestEgit {
+
+}
